40bd25bc7a2b0b3934419766d7d3a5f4940c34bd
wangqian <wangqian@administrator>
2018-01-05 17:39:47 +0800

    Signed-off-by: wangqian <wangqian@administrator>

---
+++ demo/WebRoot/views/sjzd/form_add.js
@@ -0,0 +1,52 @@
+﻿$(function(){
+	$("#FID").val(localStorage.FID);
+	$("#id").val(localStorage.sjzdmxID);
+	if(localStorage.FormMode=="Edit"){
+		var json = { "ID": localStorage.sjzdmxID};
+        var __str = JSON.stringify(json);
+		$.ajax({
+			url:"../../getDatadictionary_mx.do",
+			type:"POST",
+			data:{"v_json":__str},
+			dataType:"JSON",
+			async:false,
+			success:function(data){
+				console.log(data);
+				$("#frmdata").fill(data[0], { styleElementName: 'none' });
+				$("input[name=是否禁用]").attr("checked",data[0].是否禁用); 
+				$("#id").val(data[0].ID);
+			}
+		});
+	}
+	$("#btntable-save").click(function(){
+		var json = {};
+		var action="";
+		var data;
+		if(localStorage.FormMode=="Add"){
+			action="C";
+		}else{
+	        action="M";
+		}
+		data = $("#frmdata").serializeObject();
+		json.table = $("#frmdata").data("table");
+        json.tabledata = data;
+        FormAction(json,action);
+	});
+	function FormAction(data,action){
+		 var __str= JSON.stringify(data);
+		 $.ajax({
+				type:"POST",
+				url:"../../SingleJson.do",
+				data:{"v_json":__str,"action":action},
+				dataType:"JSON",
+				async:false,
+				success:function(data){
+					if(data.json.State==1){
+						layer.msg("操作成功");
+					}else{
+						layer.msg("操作失败");
+					}
+				}
+			});
+	 }
+});
